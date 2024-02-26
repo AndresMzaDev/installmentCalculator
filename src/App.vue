b01bd020ede9f55772536f22d5b6bb94eb532c23
AndresMzaDev <andes_nmeza@hotmail.com>
2024-02-25 19:52:47 -0600

    modulo alerta creado

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import Instructions from "./components/Instructions.vue";
 // libreria de formato numerico
 import { directive as VNumber } from "@coders-tm/vue-number-format";
@@ -23,6 +23,7 @@ const rate = {
   masked: false,
 };
 
+const isCompleteForm = ref(false);
 const form = reactive({
   amount: null,
   interestRate: null,
@@ -31,6 +32,14 @@ const form = reactive({
 
 function calculate() {
   console.log("Se esta calculando");
+  moduleManager();
+}
+
+function moduleManager() {
+  isCompleteForm.value = true;
+  setTimeout(() => {
+    isCompleteForm.value = false;
+  }, 3000);
 }
 </script>
 
@@ -134,28 +143,10 @@ function calculate() {
             placeholder="Plazo de préstamo (meses)"
             class="w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]"
           />
-
-          <div
-            class="flex bg-red-100 rounded-lg p-4 mb-4 text-sm text-red-700"
-            role="alert"
-          >
-            <svg
-              class="w-5 h-5 inline mr-3"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <div>
-              <span class="font-medium">Error!</span>
-              <div>Ingrese todos los datos solicitados.</div>
-            </div>
-          </div>
+        
+          <Transition mode="out-in" name="bounce">
+              
+          </Transition>
 
           <button
             type="button"
@@ -174,16 +165,21 @@ function calculate() {
 img {
   width: 5rem;
 }
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.bounce-leave-active {
+  animation: bounce-in 0.5s reverse;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
